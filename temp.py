fa56a6725f2df1c961c91b655e672f804d60c885
HUY <huyhunhngc@gmail.com>
2020-12-28 18:13:27 +0700

    setting

---
+++ temp.py
@@ -0,0 +1,40 @@
+import copy
+import json
+import random
+
+a = {
+  "model": "jobs.joblisting",
+  "pk": 1,
+  "fields": {
+    "title": "Frond end developer",
+    "job_location": "Đà Nẵng, Việt Nam",
+    "employment_status": "Full Time",
+    "application_deadline": "2020-12-29T00:00:00Z",
+    "gender": "Female",
+    "responsibilities": "Trách nhiệm",
+    "experience": "làm được là được",
+    "Salary": "250$",
+  }
+}
+
+luu = []
+location = ["Đà Nẵng, Việt Nam", "TP.HCM, Việt Nam", "Hà Nội, Việt Nam", "Bình Dương, Việt Nam"]
+title = ["Frond end developer", "Back end developer", "Designer developer", "Network Engineering", "Mobile Developer"]
+employment_status = ['Part Time', 'Full Time','Freelancer']
+gender = ['Male', 'Female','Any']
+
+for i in range(100):
+    a["model"] = "jobs.joblisting"
+    a["pk"] = i+1
+    for _ in range(4):
+        a["fields"]["title"] = title[random.randint(0,4)]
+        a["fields"]["job_location"] = location[random.randint(0,3)]
+        a["fields"]["employment_status"] = employment_status[random.randint(0,2)]
+        a["fields"]["gender"] = gender[random.randint(0,2)]
+        a["fields"]["Salary"] = str(random.randint(100,1000))
+        a["fields"]["application_deadline"] = str(random.randint(2018,2020)) + "-" + str(random.randint(1,12)) + "-" + str(random.randint(1,25)) + "T00:00:00Z"
+    luu.append(copy.deepcopy(a))
+
+
+with open('data2.json', 'w') as outfile:
+    json.dump(luu, outfile)
